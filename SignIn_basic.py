798339db074b516a935eee6b854ce7b425d41cc7
Pranai phoprasat <141810435+Sunbright53@users.noreply.github.com>
2025-03-18 18:13:19 +0700

    Update SignIn_basic.py

--- SignIn_basic.py
+++ SignIn_basic.py
@@ -1,14 +1,17 @@
 import time
-add_user = input("Username: ")
-add_pass = input("Password: ")
 
-if add_user == "admin" and add_pass == "1111":
-    print (f'กำลังเข้าสู่ระบบ')
-    for i in range(10,0,-1):
-        print (i)
-        time.sleep(1)
-    print(f'เข้าสู่ระบบ')
-else:
-    print (f'คุณกรอกรหัสไม่ถูกต้อง')
+def ad_login():
+    add_user = input("Username: ")
+    add_pass = input("Password: ")
+    def iflog():
+        if add_user == "admin" and add_pass == "1111":
+            print (f'กำลังเข้าสู่ระบบ')
+            for i in range(10,0,-1):
+                print (i)
+                time.sleep(1)
+            print(f'เข้าสู่ระบบ')
+        else:
+            print (f'คุณกรอกรหัสไม่ถูกต้อง')
+    iflog()
 
-
+ad_login()
