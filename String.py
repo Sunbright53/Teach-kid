1153f3d228342a14ffade3f1902288cebe96f38f
Pranai phoprasat <141810435+Sunbright53@users.noreply.github.com>
2025-03-18 22:10:32 +0700

    Create String.py

---
+++ String.py
@@ -0,0 +1,18 @@
+name_ani = "Pan"+"Da" #mix2string
+print(f'{name_ani}') #formattedstring
+
+draw = '''
+Pranai
+_____       _____
+    +       +    
+        __
+'''
+print(f'This is drawing {draw}')
+#three double quotes
+
+
+name = "Pranai"
+print(name[2:5])
+print(name[3:])
+print(name[:4])
+#slice
