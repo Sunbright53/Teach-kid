c0e2e6217a58b4353f1284fc99153edba0c682fc
Pranai phoprasat <141810435+Sunbright53@users.noreply.github.com>
2025-03-18 08:50:50 +0700

    Create loop.py

---
+++ Input/loop.py
@@ -0,0 +1,21 @@
+# from random import randint
+# def re_loop():
+
+#     put_cal = int(input("กรุณาใส่ค่าที่ต้องการหา: "))
+
+#     for i in range(1,24):
+#         print("ได้ค่าของ", put_cal, "X", i, "เท่ากับ", i * put_cal)
+    
+    
+
+# re_loop()
+# from datetime import datetime
+# print(datetime.now())
+
+
+
+
+
+
+
+
