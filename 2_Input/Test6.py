8effafa0b59225ff27eb0be739f4f1f972a423ca
Pranai phoprasat <141810435+Sunbright53@users.noreply.github.com>
2025-03-18 08:48:27 +0700

    Create Test6.py

---
+++ 2_Input/Test6.py
@@ -0,0 +1,48 @@
+
+
+# import random
+
+# rando = int(input("ใส่ค่าที่คุณต้องการ (1-10):"))
+# i = 0
+# while True:
+#     rand_num = random.randint(1,10)
+#     i+=1
+#     print("สุ่มได้: ", rand_num)
+
+#     if rand_num == rando:
+#         print("เจอแล้วเลข", rand_num, "ของคุณ ใช้การสุ่มทั้งหมด", i, "ครั้ง")
+#         break
+    
+
+# def add_one_and_one():
+#     x = 1 + 1
+#     print(x)
+
+# add_one_and_one()
+
+# def add_one_and_one():
+#      return 1 + 1
+
+# print(add_one_and_one())
+
+# from random import randint
+# fire = "Nice"
+# def roll_dice():
+#     max = input('How many sides on your dice?:')
+#     print(f'That is a D {max}')
+#     roll = randint(1, int(max))
+#     print(f'You rolled a {roll} {fire * roll}')
+# (roll_dice())
+
+# from random import randint
+# def test():
+#     reci = int(input('Correct the number of your favorite (1-6):'))
+#     ran = randint(1,6)
+#     if ran == reci:
+#         print(f'The number is {ran}')
+#         print(f'You win')
+#     else:
+#         print(f'The number is {ran}')
+#         print(f'You lose')
+# test()
+
